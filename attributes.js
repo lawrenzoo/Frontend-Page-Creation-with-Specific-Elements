2cc1b8efc2c0b1ecbfa602ba3cbe4193f2421807
lawrenzoo <chiagozielawrence@gmail.com>
2023-09-08 20:30:45 +0100

    thirteeth commit

--- attributes.js
+++ attributes.js
@@ -1,13 +1,14 @@
 
-function updateDateTime() {
+function updateDate() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     const currentDate = new Date();
-    const dayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+    let currentUTC = currentDate.getUTCDay()
+    const dayOfWeek = daysOfWeek[currentUTC];
     const currentTime = Date.now();
     const FormatedUTC = currentTime.toLocaleString();
     
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day of the Week: ${dayOfWeek}`;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${FormatedUTC}`;
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfWeek;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent = `${FormatedUTC}Milliseconds`;
 }
- updateDateTime();
+ updateDate();
