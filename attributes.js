cf5ae0cdd153eb889ebbcc4360ea2ec53892245b
lawrenzoo <chiagozielawrence@gmail.com>
2023-09-08 22:16:11 +0100

    firth commit

--- attributes.js
+++ attributes.js
@@ -6,7 +6,7 @@ function updateDate() {
     let currentUTC = currentDate.getUTCDay()
     const dayOfWeek = daysOfWeek[currentUTC];
     const currentTime = Date.now();
-    const FormatedUTC = currentTime.toLocaleString();
+
     
     document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = dayOfWeek;
     document.querySelector('[data-testid="currentUTCTime"]').textContent = `${FormatedUTC}Milliseconds`;
